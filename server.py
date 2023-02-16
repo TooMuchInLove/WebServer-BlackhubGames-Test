00d754673ea680e9911f5ad6874b116f844ed616
Fyodorov Dmitry <59337840+TooMuchInLove@users.noreply.github.com>
2023-02-16 18:52:59 +0300

    upd server

--- server.py
+++ server.py
@@ -1,20 +1,24 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
+# Аннотации типов
+from typing import List
+# Программный интерфейс для обеспечения обмена данными между процессами
 from socket import socket, gethostbyname
 from socket import AF_INET, SOCK_STREAM
-from typing import List
-
+# Конфигурация + локальные модули
 from config import SERVER_ADDRESS, SERVER_START
 from config import HOSTNAME, PROTOCOL
-from parser import get_links_html
+from parser import read_html_remote, read_html_local, get_links_html
+# html-шаблон
+from template import HTML
 
 
 # socket (сервер/клиент)
-ServerSocket = socket
-ClientSocket = socket
+SERVERObject = socket
+CLIENTObject = socket
 # Список полученных данных
-DataList = List[str]
+DATAList = List[str]
 
 
 def create_server() -> None:
@@ -28,7 +32,7 @@ def create_server() -> None:
     _start_server(server)
 
 
-def _start_server(_server: ServerSocket) -> None:
+def _start_server(_server: SERVERObject) -> None:
     # Запуск сервера
     while SERVER_START:
         # Принимаем подключение от сервера (клиентский сокет и адрес)
@@ -39,7 +43,7 @@ def _start_server(_server: ServerSocket) -> None:
         _close_server(client)
 
 
-def _manipulation_data(_client: ClientSocket) -> None:
+def _manipulation_data(_client: CLIENTObject) -> None:
     # Чтение запрошенных данных
     request = _read_data(_client)
     # Ответ от сервера
@@ -48,7 +52,7 @@ def _manipulation_data(_client: ClientSocket) -> None:
     _send_data(_client, response)
 
 
-def _read_data(_client: ClientSocket) -> DataList:
+def _read_data(_client: CLIENTObject) -> DATAList:
     # Чтение запрошенных данных от клиента
     request = _client.recv(1024).decode().split('\n')
     print(request, '\n')
@@ -56,30 +60,12 @@ def _read_data(_client: ClientSocket) -> DataList:
     return request
 
 
-def _response_server(_request: DataList) -> str:
+def _response_server(_request: DATAList) -> str:
     # Список url-адресов
-    urls = get_links_html(PROTOCOL+HOSTNAME)
-    print(urls)
+    #urls = get_links_html(PROTOCOL+HOSTNAME)
+    urls = ''
     # Формируем ответ от сервера
-    response = '''
-    <!DOCTYPE html>
-    <html lang="ru">
-    <head>
-    <meta charset="UTF-8">
-    <title>Home</title>
-    <style type="text/css">
-    body {padding: 10px 50px;}
-    h1 {font-size: 30px; padding: 10px 20px;}
-    h3 {font-size: 20px; font-family: monospace;}
-    i {color: #ff0000;}
-    </style>
-    '''
-    response += '</head>'
-    response += '<body>'
-    response += '<header>'
-    response += '<h1>Тестовое задание: Middle Python Developer в BlackHub Games</h1>'
-    response += '</header>'
-    response += '<main>'
+    response = ''
     if _request != ['']:
         # Метод, путь, протокол, localhost
         method, path, protocol = _request[0].split()
@@ -99,18 +85,20 @@ def _response_server(_request: DataList) -> str:
 
         response += '</h3>'
         response += '<h3><i>Host:</i> '+gethostbyname(HOSTNAME)+'</h3>'
-    response += '</main>'
-    response += '</body></html>'
+    # ...
+    # index = HTML.find('</main>')
+    # HTML = HTML[:index] + response + HTML[index:]
+    # print(HTML)
     # Возвращаем результат :param str: html
     return response
 
 
-def _send_data(_client: ClientSocket, _response: str) -> None:
+def _send_data(_client: CLIENTObject, _response: str) -> None:
     # Формируем отправку данных клиенту
     _client.send(_response.encode())
 
 
-def _close_server(_client: ClientSocket) -> None:
+def _close_server(_client: CLIENTObject) -> None:
     # Формируем закрытие соединения с сервером
     _client.close()
 
